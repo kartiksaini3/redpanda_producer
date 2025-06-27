5404faede5ca02f4ef1d7f5c0bdb9431e5576021
Kartik_ATR923 <kartik.saini@antiersolutions.com>
2025-06-27 13:14:51 +0530

    updates

---
+++ blocks/service.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { produceMessage } from "../utils/functions.js";
+
+export const getBlocks = async (limit, res, producer, rpcUrl) => {
+  try {
+    // fetch latest block height
+    const statusRes = await axios.post(rpcUrl, {
+      jsonrpc: "2.0",
+      id: 1,
+      method: "status",
+      params: [],
+    });
+
+    const latestHeight = +statusRes.data.result.sync_info.latest_block_height;
+    await produceMessage(producer, "latest_block_number", `${latestHeight}`);
+
+    let startHeight = 1;
+    startHeight = limit
+      ? Math.max(latestHeight - limit + 1, startHeight)
+      : startHeight;
+
+    for (let height = startHeight; height <= latestHeight; height++) {
+      // fetch block info
+      const blockRes = await axios.post(rpcUrl, {
+        jsonrpc: "2.0",
+        id: height,
+        method: "block",
+        params: { height: `${height}` },
+      });
+
+      await produceMessage(
+        producer,
+        "block_info",
+        JSON.stringify(blockRes?.data)
+      );
+      await produceMessage(producer, "current_block_number", `${height}`);
+    }
+    res.json({ message: "Blocks produced" });
+  } catch (err) {
+    console.error("Error fetching blocks:", err.message);
+    res.status(500).json({ error: "Failed to fetch blocks" });
+  }
+};
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import "dotenv/config";
 import express from "express";
-import axios from "axios";
 import { Kafka } from "kafkajs";
-import { produceMessage } from "./utils/functions.js";
+import { getBlocks } from "./blocks/service.js";
 
 const kafka = new Kafka({
   clientId: "node-app",
@@ -19,45 +18,7 @@ const rpcUrl = process.env.RPC_URL;
 
 app.get(["/blocks", "/blocks/:limit"], async (req, res) => {
   const limit = +req.params.limit || null;
-
-  try {
-    // fetch latest block height
-    const statusRes = await axios.post(rpcUrl, {
-      jsonrpc: "2.0",
-      id: 1,
-      method: "status",
-      params: [],
-    });
-
-    const latestHeight = +statusRes.data.result.sync_info.latest_block_height;
-    await produceMessage(producer, "latest_block_number", `${latestHeight}`);
-
-    let startHeight = 1;
-    startHeight = limit
-      ? Math.max(latestHeight - limit + 1, startHeight)
-      : startHeight;
-
-    for (let height = startHeight; height <= latestHeight; height++) {
-      // fetch block info
-      const blockRes = await axios.post(rpcUrl, {
-        jsonrpc: "2.0",
-        id: height,
-        method: "block",
-        params: { height: `${height}` },
-      });
-
-      await produceMessage(
-        producer,
-        "block_info",
-        JSON.stringify(blockRes?.data)
-      );
-      await produceMessage(producer, "current_block_number", `${height}`);
-    }
-    res.json({ message: "Blocks produced" });
-  } catch (err) {
-    console.error("Error fetching blocks:", err.message);
-    res.status(500).json({ error: "Failed to fetch blocks" });
-  }
+  return await getBlocks(limit, res, producer, rpcUrl);
 });
 
 app.listen(port, () => {
